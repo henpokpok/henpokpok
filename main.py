db4ddbdffbbb173109dde5aac6987da0fbcabaf6
henny <91886375+henpokpok@users.noreply.github.com>
2021-10-27 14:46:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+
+import discord 
+from discord.ext import commands
+from datetime import datetime, timezone, timedelta
+import table
+#import pytz
+import os
+from dotenv import load_dotenv
+my_secret = os.environ['TOKEN']
+
+load_dotenv('.env')
+
+bot = commands.Bot(command_prefix='a!' )
+#print(pytz.all_timezones)
+@bot.event
+async def on_ready():
+    print('{0.user}'.format(bot), 'is ready2')
+    print("==================")
+    await bot.change_presence(activity=discord.Game(name="Timetable"))
+
+@bot.event
+async def on_message(message):
+    await talk_bot(message) 
+
+@bot.event
+async def talk_bot(message):   
+    tz = timezone(timedelta(hours = 7))
+    # Date with timezone
+    date = datetime.now(tz=tz) 
+    print(date)
+    day_today = str(date.today().strftime("%A"))
+    time_start = date.today().strftime("%H:%M")  
+    if str(time_start) >= "08:10" and str(time_start) <= "08:50":
+        x = 0
+    elif str(time_start) >= "08:50" and str(time_start) <= "09:30":
+        x = 1
+    elif str(time_start) >= "09:30" and str(time_start) <= "10:10":
+        x = 2
+    elif str(time_start) >= "10:10" and str(time_start) <= "10:50":
+        x = 3
+    elif str(time_start) >= "10:50" and str(time_start) <= "11:30":
+        x = 4
+    elif str(time_start) >= "11:30" and str(time_start) <= "12:10":
+        x = 5
+    elif str(time_start) >= "12:10" and str(time_start) <= "13:00":
+        x = 6
+    elif str(time_start) >= "13:00" and str(time_start) <= "13:40":
+        x = 7
+    elif str(time_start) >= "13:40" and str(time_start) <= "14:20":
+        x = 8
+    elif str(time_start) >= "14:20" and str(time_start) <= "15:00":
+        x = 9
+    else:
+        x = 10
+    print(day_today)
+    print(time_start)
+    print(x)
+
+    if 'คาบนี้' in message.content or 'ตอนนี้' in message.content:  
+        if day_today in table.day and x < 10:
+            await message.channel.send(f"{table.day[f'{day_today}'][x]} {table.timestart['timestart'][x]} - {table.timestart['timestart'][x+1]}")
+        else:
+            await message.channel.send("อะไรของมึง กวนตีนกูหรอ")
+
+    if 'คาบต่อไป' in message.content or 'คาบหน้า' in message.content:
+        if day_today in table.day and x < 10:
+            await message.channel.send(table.day[f"{day_today}"][x+1])
+        else:
+            await message.channel.send("ไม่มีเรียน อย่าโง่")
+    
+    if 'คาบเมื่อกี้' in message.content or 'คาบที่แล้ว' in message.content:  
+        if day_today in table.day and x < 10:
+            await message.channel.send(table.day[f"{day_today}"][x-1])
+        else:
+            await message.channel.send("มึงเอ๋อหรอ")
+
+    if 'ตารางเรียน' in message.content:   
+        file = discord.File("image/ตารางเรียน.jpg")
+        await message.channel.send(file = file)
+
+    if message.content == 'a!help':
+        embed=discord.Embed(title="Help" , color=0x84c5e6)
+        embed.add_field(name="คาบนี้", value="พิมพ์ในช่องแชท", inline=False)
+        embed.add_field(name="คาบต่อไป", value="พิมพ์ในช่องแชท", inline=False)
+        embed.add_field(name="ตารางเรียน", value="พิมพ์ในช่องแชท", inline=True)
+        embed.set_thumbnail(url = bot.user.avatar_url)
+        await message.channel.send(embed=embed)
+
+
+bot.run(os.getenv("TOKEN"))
